04de76c440db6af595939e56d6b5832dd404240b
betsyann <beth.hanrahan@wsu.edu>
2020-12-07 16:27:03 -0800

    Updates

---
+++ precache-manifest.aa786b7c688e7f76f440aced74e49295.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65cfdd2086b2e34f1ed0a69635af443b",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "a6213f1e6515f99695b8",
+    "url": "/todo/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "7251807bdf112ee5b094",
+    "url": "/todo/static/css/main.73805b52.chunk.css"
+  },
+  {
+    "revision": "a6213f1e6515f99695b8",
+    "url": "/todo/static/js/2.a7cee0f5.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/todo/static/js/2.a7cee0f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7251807bdf112ee5b094",
+    "url": "/todo/static/js/main.b6df75e8.chunk.js"
+  },
+  {
+    "revision": "86504a2bbc65b8ae6c2e",
+    "url": "/todo/static/js/runtime-main.84254937.js"
+  }
+]);
